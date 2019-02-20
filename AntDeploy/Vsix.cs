fe173030f2138865703ff9f0887959e21c158682
zdyu <zdyu@Ctrip.com>
2019-02-20 13:43:38 +0800

    v2.4

--- AntDeploy/Vsix.cs
+++ AntDeploy/Vsix.cs
@@ -2,7 +2,7 @@
 {
     internal static class Vsix
     {
-        public const string VERSION = "2.3";
+        public const string VERSION = "2.4";
         public const string PRODUCT = "yuzd";
         public const string PACKAGE = "AntDeploy";
         public const string URL = "https://github.com/yuzd";
