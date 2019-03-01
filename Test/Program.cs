d4fea40286d8c6da0f457b69105c9084874fbfc5
zdyu <zdyu@Ctrip.com>
2019-03-01 10:15:11 +0800

    up

--- Test/Program.cs
+++ Test/Program.cs
@@ -14,43 +14,63 @@ namespace Test
         static void Main(string[] args)
         {
 
+            var mer = ZipHelper.DoCreateTarFromDirectory(
+                @"E:\WorkSpace\github\Lito\Lito\Lito.APP\bin\Debug\netcoreapp2.1\publish",new List<string>
+                {
+                    "/win-x86?.+",
+                    "web.config"
+                });
 
+            var filePath2= Path.Combine(@"E:\WorkSpace\github\Lito\Lito\Lito.APP\bin\Debug\netcoreapp2.1\logs", "test.tar");
+            using (var fs = new FileStream(filePath2, FileMode.Create))
+            {
+                var bt = mer.GetBuffer();
+                fs.Write(bt,0, bt.Length);
+            }
+
+            GitClient gitHelper = new GitClient(@"E:\WorkSpace\github\AntDeploy\Test\bin\Debug\testgit");
+            var s=gitHelper.GetChanges();
+
+            var s1 = ZipHelper.GetFullFileInfo(s, @"E:\WorkSpace\github\AntDeploy\Test\bin\Debug\testgit");
+             var allFile = ZipHelper.FindFileDir(@"E:\WorkSpace\github\AntDeploy\Test\bin\Debug\testgit");
+
+
+            
             //var buildResult = CommandHelper.RunMsbuild("E:\\WorkSpace\\github\\AntDeploy\\AntDeployAgentWindowsService\\AntDeployAgentWindowsService.csproj",
             //    Console.WriteLine,Console.WriteLine);
 
 
 
             //SSHClient sshClient = new SSHClient("192.168.0.7:22","root","kawayiyi@1",Console.WriteLine);
-            using (SSHClient sshClient = new SSHClient("192.168.159.131:22","root","admin",Console.WriteLine,Console.WriteLine)
-            {
-                NetCoreENTRYPOINT = "Lito.APP.dll",
-                NetCoreVersion = "2.1",
-                NetCorePort = "5004",
+            //using (SSHClient sshClient = new SSHClient("192.168.159.131:22","root","admin",Console.WriteLine,Console.WriteLine)
+            //{
+            //    NetCoreENTRYPOINT = "Lito.APP.dll",
+            //    NetCoreVersion = "2.1",
+            //    NetCorePort = "5004",
 
-            })
-            {
-                sshClient.Connect();
-                sshClient.PublishZip(@"E:\WorkSpace\github\Lito\Lito\Lito.APP\bin\Debug\netcoreapp2.1\publish\", new List<string>
-                {
-                    "web.config"
-                }, "publisher","publish.zip");
-            }
+            //})
+            //{
+            //    sshClient.Connect();w
+            //    sshClient.PublishZip(@"E:\WorkSpace\github\Lito\Lito\Lito.APP\bin\Debug\netcoreapp2.1\publish\", new List<string>
+            //    {
+            //        "web.config"
+            //    }, "publisher","publish.zip");
+            //}
             
 
 
 
             var bytes= ZipHelper.DoCreateFromDirectory(
-                @"H:\Csharp\yuzd\Lito\Lito\Lito.APP\bin\Release\netcoreapp2.1\publish",
+                @"E:\WorkSpace\github\Lito\Lito\Lito.APP\bin\Debug\netcoreapp2.1\publish\",
                 CompressionLevel.Optimal, true,new List<string>
                 {
-                    "appsettings.*",
-                    "web.config",
-                    "QRCoder.dll"
+                    "/win-x86?.+",
+                    "web.config"
                 });
 
 
             
-            var filePath = Path.Combine(@"H:\Csharp\yuzd\AntDeploy\AntDeploy\WindowsFormsAppTest\Test\MyProject\bin\Release\netstandard2.0\unzip", "aa.zip");
+            var filePath = Path.Combine(@"E:\WorkSpace\github\Lito\Lito\Lito.APP\bin\Debug\netcoreapp2.1\logs", "aa.zip");
             using (var fs = new FileStream(filePath, FileMode.Create))
             {
                 fs.Write(bytes,0, bytes.Length);
@@ -59,7 +79,7 @@ namespace Test
             //解压
             try
             {
-                ZipFile.ExtractToDirectory(filePath, @"H:\Csharp\yuzd\AntDeploy\AntDeploy\WindowsFormsAppTest\Test\MyProject\bin\Release\netstandard2.0\unzip");
+                ZipFile.ExtractToDirectory(filePath, @"E:\WorkSpace\github\Lito\Lito\Lito.APP\bin\Debug\netcoreapp2.1\logs");
             }
             catch (Exception ex)
             {
