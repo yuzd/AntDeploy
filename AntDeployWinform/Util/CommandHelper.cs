c765c1dbd7779bf9ffdbdcadee691fdc9bab65ed
zdyu <zdyu@Ctrip.com>
2019-04-22 20:11:42 +0800

    up

--- AntDeployWinform/Util/CommandHelper.cs
+++ AntDeployWinform/Util/CommandHelper.cs
@@ -43,7 +43,7 @@ namespace AntDeployWinform.Util
             var buildArg = "\"" + path.Replace("\\\\", "\\") + "\"";
             if (isWeb)
             {
-                buildArg += " /verbosity:minimal /p:Configuration=Release /p:Platform=AnyCPU /t:WebPublish /p:WebPublishMethod=FileSystem /p:DeleteExistingFiles=False /p:publishUrl=\"" + path2 + "\"";
+                buildArg += " /verbosity:minimal /p:Configuration=Release /p:DeployOnBuild=true /p:Platform=AnyCPU /t:WebPublish /p:WebPublishMethod=FileSystem /p:DeleteExistingFiles=False /p:publishUrl=\"" + path2 + "\"";
             }
             else
             {
