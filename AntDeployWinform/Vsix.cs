23fde111b930ded750f1ccee4b7768c533a330da
yuzd <zdyu@Ctrip.com>
2019-05-11 20:26:27 +0800

    新增stop功能

--- AntDeployWinform/Vsix.cs
+++ AntDeployWinform/Vsix.cs
@@ -2,7 +2,7 @@
 {
     public static class Vsix
     {
-        public const string VERSION = "4.5";
+        public const string VERSION = "4.6";
         public const string AGENTVERSION = "4.0";
         public const string PRODUCT = "yuzd";
         public const string PACKAGE = "AntDeploy";
