96cc1c571d333cb59329b40c7304af2b73e2e560
zdyu <1877682825@qq.com>
2022-09-27 23:09:26 +0800

    v7.4.5

--- AntDeployWinform/Vsix.cs
+++ AntDeployWinform/Vsix.cs
@@ -3,7 +3,7 @@
     public static class Vsix
     {
         public const string FORM_NAME = "AntDeploy("+VERSION+")";
-        public const string VERSION = "7.44";
+        public const string VERSION = "7.45";
         public const string AGENTVERSION = "7.1";
         public const string PRODUCT = "yuzd";
         public const string PACKAGE = "AntDeploy";
