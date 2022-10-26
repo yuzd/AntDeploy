9aa46a18313a8c4de301fa28e5e863dd203897d6
zdyu <zdyu@Ctrip.com>
2022-10-26 14:59:49 +0800

    fix arm64 导致的 msbuild 编译老的netframework项目异常的问题

--- AntDeployWinform/Vsix.cs
+++ AntDeployWinform/Vsix.cs
@@ -3,7 +3,7 @@
     public static class Vsix
     {
         public const string FORM_NAME = "AntDeploy("+VERSION+")";
-        public const string VERSION = "7.47";
+        public const string VERSION = "7.48";
         public const string AGENTVERSION = "7.1";
         public const string PRODUCT = "yuzd";
         public const string PACKAGE = "AntDeploy";
